32fa0c6f742652194bd0bab8a876ba13d6404edf
MaideAkdede <maide.akdede@student.hepl.com>
2021-06-04 17:51:23 +0200

    :tada: add Custom Post Type Function

--- wp-content/themes/easyspacy/functions.php
+++ wp-content/themes/easyspacy/functions.php
@@ -1,6 +1,60 @@
 <?php
+/* * * Register Multiple Custom Post Type * * */
+add_action('init', 'es_custom_post_type');
+
+function es_custom_post_type() {
+    register_post_type('capsule', [
+        'label' => 'Capsules',
+        'labels' => [
+            'singular_name' => 'Capsule',
+            'add_new' => 'Nouvelle capsule',
+            'add_new_item' => 'Ajouter une nouvelle capsule',
+        ],
+        'description' => 'Liste de toutes nos capsules',
+        'public' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-format-gallery',
+        'supports' => ['title','editor'],
+        'rewrite' => [
+            'slug' => 'capsules'
+        ]
+    ]);
+    register_post_type('news', [
+        'label' => 'News',
+        'labels' => [
+            'singular_name' => 'One News',
+            'add_new' => 'Nouvelle News',
+            'add_new_item' => 'Ajouter une nouvelle News',
+        ],
+        'description' => 'Liste de toutes nos News',
+        'public' => true,
+        'menu_position' => 6,
+        'menu_icon' => 'dashicons-admin-site-alt3',
+        'supports' => ['title','editor'],
+        'rewrite' => [
+            'slug' => 'news'
+        ]
+    ]);
+    register_post_type('presse', [
+        'label' => 'Presse',
+        'labels' => [
+            'singular_name' => 'presse',
+            'add_new' => 'Nouvelle revue de presse',
+            'add_new_item' => 'Ajouter une nouvelle revue de presse',
+        ],
+        'description' => 'Liste de toutes nos revues de presse',
+        'public' => true,
+        'menu_position' => 7,
+        'menu_icon' => 'dashicons-camera',
+        'supports' => ['title'],
+        'rewrite' => [
+            'slug' => 'presse'
+        ]
+    ]);
+}
+
 /* * * Return a compiled assets URI * * */
-function dw_assets($path)
+function es_assets($path)
 {
     return rtrim(get_template_directory_uri(), '/') . '/public/' . ltrim($path, '/');
 }
