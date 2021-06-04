58a76c38e38c69e5aa0d7f3bff11ccacce32448e
MaideAkdede <maide.akdede@student.hepl.com>
2021-06-04 15:53:03 +0200

    :tada: add contact Template

--- wp-content/themes/easyspacy/template-contact.php
+++ wp-content/themes/easyspacy/template-contact.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <main class="contact">
         <div class="contact__header">
-            <h1 class="contact__title"><?php the_title(); ?></h1>
+            <h2 class="contact__title"><?php the_title(); ?></h2>
             <div class="contact__wysiwyg"><?php the_content(); ?></div>
         </div>
         <div class="contact__content">
